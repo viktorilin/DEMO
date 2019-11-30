fdf38601adbfe1c99b5574bdff4c4ee29d6b2c8f
viktor <viktor140798@gmail.com>
2019-11-30 14:38:25 +0200

    first commit

---
+++ DEMO/Request.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System.IO;
+using System.Net;
+
+namespace DEMO
+{
+    public static class Request
+    {
+        public static void req()
+        {
+            var client = new RestClient("http://localhost:8083");
+            client.Authenticator = new HttpBasicAuthenticator("viktor", "vi140798il");
+
+            var request = new RestRequest("/rest/raven/1.0/import/execution/nunit?projectKey=XRAYDEMO", Method.POST);
+           
+            var path = @"E:\DEMO\DEMO\TestResult.xml";
+
+            // add files to upload (works with compatible verbs)
+            request.AddFile("file", path);
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+        }
+    }
+}
---
+++ DEMO/WdriverTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DEMO
+{
+    [TestFixture]
+    public class WdriverTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+        }
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+                Request.req();
+            }
+            catch (Exception e)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+        [Test, Property("Requirement", "CALC-1")]
+        public void GoogleTest()
+        {
+            // Open Google search engine.
+            driver.Navigate().GoToUrl("http://www.google.com/");
+            // Assert Title of page.
+            Assert.AreEqual("Google", driver.Title);
+            // Provide search term as "Selenium OpenQA"
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Selenium OpenQA");
+            query.Submit();
+            Assert.IsTrue(driver.PageSource.Contains("www.openqa.org"));
+            Assert.AreEqual("Selenium OpenQA - Google Search", driver.Title);
+        }
+    }
+}
