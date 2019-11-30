44d5dde3bd6e41eeb973f624cd0678a516b4518a
viktor <viktor140798@gmail.com>
2019-11-30 15:47:05 +0200

    added API

---
+++ API/Program.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://localhost:8083");
+            client.Authenticator = new HttpBasicAuthenticator("viktor", "vi140798il");
+
+            var request = new RestRequest("/rest/raven/1.0/import/execution/nunit?projectKey=XRAYDEMO", Method.POST);
+
+            string PATH = System.Reflection.Assembly.GetCallingAssembly().CodeBase;
+            string ACTUAL_PATH = PATH.Substring(0, PATH.LastIndexOf("bin"));
+            string PROJECT_PATH = new Uri(ACTUAL_PATH).LocalPath;
+
+            string path = $@"{PROJECT_PATH}..\TestResult.xml";
+
+
+            // add files to upload (works with compatible verbs)
+            request.AddFile("file", path);
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+        }
+    }
+}
